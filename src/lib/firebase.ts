3f1e201127ff5c9d3641b74d1d3b768e37e5a58f
Ulum <uniquefactuhl@gmail.com>
2026-08-13 13:02:56 +0700

    config: switch to menfess-8f2b7 Firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-// @ts-ignore
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4lEQfpXosKTLbpv25Tsdb_YZdBXSV5pg",
+  authDomain: "menfess-8f2b7.firebaseapp.com",
+  projectId: "menfess-8f2b7",
+  storageBucket: "menfess-8f2b7.firebasestorage.app",
+  messagingSenderId: "876546975569",
+  appId: "1:876546975569:web:61c92d9bda5e3bb2771570",
+  measurementId: "G-HSG2S5J296"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
